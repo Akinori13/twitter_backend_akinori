daad577f315a7f42933e3066d18ab30944478b0e
akino <akikoba2001@gmail.com>
2021-01-25 22:30:40 +0900

    Issue #7 Add path

--- tweets/urls.py
+++ tweets/urls.py
@@ -3,5 +3,12 @@ from . import views
 
 app_name = 'tweets'
 urlpatterns = [
+    # Read
     path('timeline', views.timeline, name='timeline'),
+    # Create
+    path('create', views.create, name='create'),
+    # Update
+    path('edit', views.edit, name='edit'),
+    # Delete
+    path('delete', views.delete, name='delete'),
 ]
