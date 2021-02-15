2c1bebcb6f406b58aa082c43bb5ec91e18f963b6
akino <akikoba2001@gmail.com>
2021-02-15 16:04:41 +0900

    #10 Make Connect(path)

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('', include('django.contrib.auth.urls')),
+    path('<int:user_id>/followers', views.FollowIndexView.as_view(), name='follow_index'),
+    path('<int:user_id>/follow', views.FollowCreateView.as_view(), name='follow_create'),
+    path('<int:user_id>/follow/delete', views.FollowDeleteView, name='follow_delete'),
 ]
