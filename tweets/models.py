54c3d9ab7238e18f3c2753c0a09a25a7814b3b81
akino <akikoba2001@gmail.com>
2021-02-19 15:20:13 +0900

    #12 Create like:model and migrate

--- tweets/models.py
+++ tweets/models.py
@@ -9,3 +9,22 @@ class Tweet(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True) # 登録タイムスタンプ
     updated_at = models.DateTimeField(auto_now=True) # 更新タイムスタンプ
+
+class Like(models.Model):
+    user = models.ForeignKey(
+        'accounts.User',
+        on_delete=models.CASCADE,
+    )
+    tweet = models.ForeignKey(
+        Tweet,
+        on_delete=models.CASCADE,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields = ['user', 'tweet'],
+                name = 'user_tweet_unique'
+            )
+        ]
